350b4bf88f98d4002d723911b68f91922ad2bfe2
UzhvenkoJulia <144743928+UzhvenkoJulia@users.noreply.github.com>
2024-06-09 00:47:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-# main.py додаємо зміни також2
-from utils import factorial
+# main.py додаємо зміни також2 до кожно прописаною мною функцією у utils.py
+from utils import gcd, degree_of_five, is_prime, factorial
 
+print(degree_of_five(125))
+print(gcd(8, 12))
+print(is_prime(3))
 print(factorial(5))
