57ed33c44b0607f3e4247e7c093939ed656f4132
UzhvenkoJulia <144743928+UzhvenkoJulia@users.noreply.github.com>
2024-06-09 00:46:41 +0300

    Add all functions to utils.py and update main.py

--- utils.py
+++ utils.py
@@ -1,6 +1,30 @@
-# utils.py 
+# utils.py додаю з кожним разом нову функцію
+
+def degree_of_five(numb):
+    if numb <= 0:
+        return False
+    elif numb == 1:
+        return True
+    while numb % 5 == 0:
+        numb /= 5
+    return numb == 1
+
+
+def gcd(x, y):
+    while y != 0:
+        x, y = y, x % y
+    return x
+
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
 def factorial(n):
-    if n == 0:
+    if n == 0 or n == 1:
         return 1
     else:
-        return n * factorial(n-1)
+        return n * factorial(n - 1)
